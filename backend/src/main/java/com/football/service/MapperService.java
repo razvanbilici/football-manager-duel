9cc90cc4c1af0596a40e58a8b690a03c3268b51a
Andrei Turcu <andrei.turcu@example.com>
2026-06-26 02:11:55 +0300

    fix part 2

--- backend/src/main/java/com/football/service/MapperService.java
+++ backend/src/main/java/com/football/service/MapperService.java
@@ -6,7 +6,6 @@ import com.football.repository.PlayerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
